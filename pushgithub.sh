cd20ff92c3f15fdf8d8085551d18df7705c4f8af
Travis CI <travis@travis-ci.org>
2018-03-12 13:55:55 +0000

    udpate

--- pushgithub.sh
+++ pushgithub.sh
@@ -11,8 +11,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/sudtanj/MPICH-Ubuntu > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+  git push 
 }
 
 setup_git
