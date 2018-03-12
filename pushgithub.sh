5b295a03b6f9f2d5cc4f2d29b0603d8ff2be35ad
Travis CI <travis@travis-ci.org>
2018-03-12 14:02:47 +0000

    udpate

--- pushgithub.sh
+++ pushgithub.sh
@@ -3,6 +3,7 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git config remote.origin.url https://$GH_TOKEN@github.com/sudtanj/MPICH-Ubuntu.git
 }
 
 commit_website_files() {
