a08c96afa0cf3ae98ee67de97498ae6bdc1bdb11
Sudono Tanjung <sudono007@gmail.com>
2018-03-12 13:40:05 +0000

    update

--- pushgithub.sh
+++ pushgithub.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/sudtanj/MPICH-Ubuntu > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git push --quiet --set-upstream origin-pages master
 }
 
 setup_git
